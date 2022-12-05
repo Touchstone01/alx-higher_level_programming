3be6f6f2128c99d62f1a24955f993dc68bb20437
[Lateefat Shuaib] <[ayomidelateefat23@gmail.com]>
2022-12-05 18:10:58 +0100

    Help

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-def new_in_list(my_list, idx, element):
-    if my_list:
-        if idx < 0 or idx >= len(my_list):
-            return (my_list)
-        else:
-            new_list = my_list.copy()
-            new_list[idx] = element
-            return (new_list)
+def no_c(my_string):
+    remove_c_C = ''
+    for ch in my_string:
+        if ch != 'c' and ch != 'C':
+            remove_c_C += ch
+    return (remove_c_C)
